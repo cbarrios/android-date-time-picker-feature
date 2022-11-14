a7b829fdc9a31e4d219f3d6f367f2431a8c71ce6
cbarrios <lalobarrios16@yahoo.com>
2022-11-14 13:40:39 -0500

    master: (3) picker dialogs

--- app/src/main/java/com/lalosapps/datetimepickerdialogs/MainActivity.kt
+++ app/src/main/java/com/lalosapps/datetimepickerdialogs/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.lalosapps.datetimepickerdialogs
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.lalosapps.datetimepickerdialogs.ui.theme.DateTimePickerDialogsTheme
+import com.vanpra.composematerialdialogs.MaterialDialog
+import com.vanpra.composematerialdialogs.datetime.date.datepicker
+import com.vanpra.composematerialdialogs.datetime.time.timepicker
+import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
@@ -32,6 +43,62 @@ class MainActivity : ComponentActivity() {
                 val formattedTime by remember {
                     derivedStateOf {
                         DateTimeFormatter.ofPattern("hh:mm").format(pickedTime)
+                    }
+                }
+
+                val dateDialogState = rememberMaterialDialogState()
+                val timeDialogState = rememberMaterialDialogState()
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Button(onClick = { dateDialogState.show() }) {
+                        Text(text = "Pick date")
+                    }
+                    Text(text = formattedDate)
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = { timeDialogState.show() }) {
+                        Text(text = "Pick time")
+                    }
+                    Text(text = formattedTime)
+                }
+                MaterialDialog(
+                    dialogState = dateDialogState,
+                    buttons = {
+                        positiveButton(text = "Ok") {
+                            Toast.makeText(applicationContext, "Clicked ok", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                        negativeButton("Cancel")
+                    }
+                ) {
+                    datepicker(
+                        initialDate = LocalDate.now(),
+                        title = "Pick a date",
+                        allowedDateValidator = {
+                            it.dayOfMonth % 2 == 1
+                        }
+                    ) {
+                        pickedDate = it
+                    }
+                }
+                MaterialDialog(
+                    dialogState = timeDialogState,
+                    buttons = {
+                        positiveButton(text = "Ok") {
+                            Toast.makeText(applicationContext, "Clicked ok", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                        negativeButton("Cancel")
+                    }
+                ) {
+                    timepicker(
+                        initialTime = LocalTime.NOON,
+                        title = "Pick a time",
+                        timeRange = LocalTime.MIDNIGHT..LocalTime.NOON
+                    ) {
+                        pickedTime = it
                     }
                 }
             }
