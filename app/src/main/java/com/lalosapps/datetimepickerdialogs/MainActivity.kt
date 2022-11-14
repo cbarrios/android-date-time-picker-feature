e844c0c919e76af65a4874053585357f33f93d49
cbarrios <lalobarrios16@yahoo.com>
2022-11-14 11:26:23 -0500

    master: (2) dependencies + main states

--- app/src/main/java/com/lalosapps/datetimepickerdialogs/MainActivity.kt
+++ app/src/main/java/com/lalosapps/datetimepickerdialogs/MainActivity.kt
@@ -3,14 +3,37 @@ package com.lalosapps.datetimepickerdialogs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
 import com.lalosapps.datetimepickerdialogs.ui.theme.DateTimePickerDialogsTheme
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DateTimePickerDialogsTheme {
-
+                var pickedDate by remember {
+                    mutableStateOf(LocalDate.now())
+                }
+                var pickedTime by remember {
+                    mutableStateOf(LocalTime.NOON)
+                }
+                val formattedDate by remember {
+                    derivedStateOf {
+                        DateTimeFormatter.ofPattern("MMM dd yyyy").format(pickedDate)
+                    }
+                }
+                val formattedTime by remember {
+                    derivedStateOf {
+                        DateTimeFormatter.ofPattern("hh:mm").format(pickedTime)
+                    }
+                }
             }
         }
     }
